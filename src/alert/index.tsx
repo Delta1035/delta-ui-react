9110d0396629987e9f901030ac95a2dd7b47e1d8
delta <528491526@qq.com>
2025-08-19 15:38:08 +0800

    feat(alert): 准备工作

---
+++ src/alert/index.tsx
@@ -0,0 +1,27 @@
+import React,{ PropsWithChildren } from 'react';
+import t from 'prop-types';
+export interface AlertProps {
+  kind?: 'info' | 'positive' | 'negative' | 'warning';
+}
+
+export type KindMap = Record<Required<AlertProps>['kind'],string>;
+
+const prefixClass = 'hp-alert';
+
+const kinds: KindMap = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+};
+
+const Alert: React.FC<PropsWithChildren<AlertProps>> = ({ children,kind = 'info',...rest }) => {
+  return <div className={prefixClass} style={{ background: kinds[kind] }} {...rest}>
+    {children}
+  </div>;
+};
+
+Alert.propTypes = {
+  kind: t.oneOf(['info','positive','negative','warning']),
+};
+export default Alert;
