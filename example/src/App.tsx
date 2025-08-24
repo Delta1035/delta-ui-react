9bdfcebefec06961d664f713793f19599c296cab
delta <528491526@qq.com>
2025-08-25 00:43:14 +0800

    chore(example): 增加example引入alert组件的示例

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react';
 import './App.css';
 import { Alert } from 'delta-ui-demo';
+import "delta-ui-demo/esm/alert/style/css.js";
+// import "delta-ui-demo/esm/alert/style/index.js";// 提示需要安装less
 function App () {
   // const [count,setCount] = useState(0);
 
   return (
     <>
-      <Alert></Alert>
+      <Alert kind='warning'>123</Alert>
     </>
   );
 }
