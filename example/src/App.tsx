cde9607984e4753683a3a754a4d2e81a5a252599
delta <528491526@qq.com>
2025-08-25 00:35:27 +0800

    chore(example): 增加测试项目

---
+++ example/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+import './App.css';
+import { Alert } from 'delta-ui-demo';
+function App () {
+  // const [count,setCount] = useState(0);
+
+  return (
+    <>
+      <Alert></Alert>
+    </>
+  );
+}
+
+export default App;
